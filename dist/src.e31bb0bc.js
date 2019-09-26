59f7d7f50f00a0b9b442603de288fcaa7c31129b
ari <ajb871@nyu.edu>
2019-09-26 12:07:49 +0800

    added multipose reading and cleaned code

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -148,7 +148,7 @@ var video;
 var osc; //Pose vars
 
 var preveyePos;
-var prevPose;
+var prevPoses = [];
 
 function setSketch(sketch) {
   p5 = sketch;
@@ -170,18 +170,7 @@ function setup() {
 
   label = p5.createDiv();
   label.position(5, height + 50);
-  label.style('font-size', '12pt'); //init audio button
-
-  var audioButton = p5.createButton('click to start audio');
-  audioButton.position(5, height + 100);
-  audioButton.mouseClicked(function () {
-    p5.userStartAudio().then(function () {
-      audioButton.remove();
-      osc = new window.p5.Oscillator(); // now you can call osc methods to make sound happen
-
-      osc.start();
-    });
-  });
+  label.style('font-size', '12pt');
 }
 
 function draw() {}
@@ -189,10 +178,12 @@ function draw() {}
 function drawPoses(poses) {
   p5.translate(width, 0); // move to far corner
 
-  p5.scale(-1.0, 1.0); //p5.tint(255, 100);
+  p5.scale(-1.0, 1.0); // Uses poses number to alter background
 
+  var numPoses = poses.length;
+  p5.tint(255, 255 / numPoses);
   p5.image(video, 0, 0, video.width, video.height); //p5.fill(0,10);
-  // p5.rect(0,0,width,height);
+  //p5.rect(0,0,width,height);
 
   readSpeed(poses); //drawKeypoints(poses);
   //drawSkeleton(poses);
@@ -205,30 +196,37 @@ function drawPoses(poses) {
 
 
 function readSpeed(poses) {
+  //Debugging to read no. of poses
+  console.log(poses.length);
+
   if (poses.length == 0) {
     //return if no poses
     return;
-  } //get vars from pose[0]
+  } //(Org testing)get vars from pose[0]
+  //const pose = poses[0];
+  //Added poses.foreach loop in order to use multipose detection
 
 
-  var pose = poses[0];
+  poses.forEach(function (pose, index) {
+    if (prevPoses[index] == undefined) {
+      prevPoses[index] = pose; //continue;
+    }
 
-  if (prevPose == undefined) {
-    prevPose = pose;
-    return;
-  }
+    pose.pose.keypoints.forEach(function (keypoint, index2) {
+      var prevKeypoint = prevPoses[index].pose.keypoints[index2]; //Calc difference (distance moved)    
 
-  pose.pose.keypoints.forEach(function (keypoint, index) {
-    var prevKeypoint = prevPose.pose.keypoints[index]; //Calc difference (distance moved)    
+      var dif = prevKeypoint.position.x - keypoint.position.x;
+      var dir; //I'm going to remember how to use vectors(?) and trig to find the direction of movement
 
-    var dif = Math.abs(prevKeypoint.position.x - keypoint.position.x);
-    var dir; //I'm going to remember how to use vectors(?) and trig to find the direction of movement
+      keypoint.dif = dif; //append this value to Pose
+      //by appening dif will be pased onto the DrawKeypoints to be easily read with a for loop
+    });
+    prevPoses[index] = pose; //set current pose to previous
+    // Call Draw Keypoints
 
-    keypoint.dif = dif; //append to pose
+    drawKeypoints(pose); //would like to replace function with "update Keypoints"
   });
-  prevPose = pose; //set current pose to previous
-
-  /* //Testing code with just leftEye position
+  /* /////////////Old Testing code with just leftEye position////////////
   const eye = pose[1];
   const eyePos = eye.position;
   label.html('LeftEye: ' + JSON.stringify(eyePos));
@@ -240,36 +238,33 @@ function readSpeed(poses) {
   let eyedif = Math.abs(preveyePos.x - eyePos.x);
   preveyePos = eyePos;
   */
-
-  drawKeypoints(pose); //would like to replace function with "update Keypoints"
-  //Next Steps:
-  //Create array of PREVIOUS pose[0](or pose[n] eventually) keypoint positions
-  //Array of vectors {(x,y),(x,y)...}
-  //use a loop to go through this array 
-  //instead of "analyzed poses" loop, just append speed,dir+distance to end of org poses array, and pass that to drawKeypoints
+  ////////////////////////////////////////////////////////////////////////
 }
 
 function drawKeypoints(pose) {
-  //pose drawing
+  //Pose drawing/graphic function
   pose.pose.keypoints.forEach(function (keypoint, index) {
     //Create vars for radius and color from dif values
     var dif = keypoint.dif;
-    var fill = p5.color(5.0 * dif, 0, 10.0 * dif);
-    var r = 5.0 + dif * 1.3;
+    var fill = p5.color(5.0 * Math.abs(dif), 0, 10.0 * Math.abs(dif)); //Speed determine fill color
+
+    var r = 5.0 + dif * 1.3; //Size -> Speed direct relationship
 
     if (keypoint.score > 0.5) {
       p5.fill(fill);
       p5.noStroke();
       p5.ellipse(keypoint.position.x, keypoint.position.y, r, r);
+      pose.pose.keypoints.forEach(function (keypoint2) {
+        p5.stroke(0, 0, Math.abs(dif) * 10.0);
+        p5.line(keypoint.position.x, keypoint.position.y, keypoint2.position.x, keypoint2.position.y);
+      });
     }
-  });
-  pose.pose.keypoints.forEach(function (keypoint) {
-    pose.pose.keypoints.forEach(function (keypoint2) {
-      p5.stroke(0, 0, keypoint.dif * 10.0);
-      p5.line(keypoint.position.x, keypoint.position.y, keypoint2.position.x, keypoint2.position.y);
-    });
-  });
-  drawEyes(pose); // pose.skeleton.forEach((skeleton) => {
+  }); // Special Draw Eyes function over top other graphics
+
+  drawEyes(pose); // pose.pose.keypoints.forEach((keypoint) => {
+  // })
+  /////////////// Old Skeleton Drawing /////////////
+  // pose.skeleton.forEach((skeleton) => {
   //       const [p1, p2] = skeleton;
   //       p5.stroke(255, 0, 0);
   //       p5.line(p1.position.x, p1.position.y, p2.position.x, p2.position.y);
@@ -278,28 +273,30 @@ function drawKeypoints(pose) {
 }
 
 function drawEyes(pose) {
-  var leftEye = pose.pose.keypoints[1].position;
-  var rightEye = pose.pose.keypoints[2].position;
-  var distance = Math.abs(leftEye.x - rightEye.x);
+  // Create array for both eyes
+  // Check for eyes (error)
+  if (!pose.pose.keypoints[1].position && !pose.pose.keypoints[2].position) {
+    return;
+  }
+
+  var eyes = [pose.pose.keypoints[1].position, pose.pose.keypoints[2].position];
+  var distance = Math.abs(eyes[0].x - eyes[1].x); //calc distance to determine size
+
   var r = distance / 2;
-  var a = Math.atan2(leftEye.y - rightEye.y, leftEye.x - rightEye.x);
-  p5.push();
-  p5.translate(leftEye.x, leftEye.y);
-  p5.rotate(a);
-  p5.fill(255);
-  p5.ellipse(0, 0, r, r / 2);
-  p5.fill(0);
-  p5.ellipse(0, 0, r / 4, r / 4);
-  p5.pop();
-  p5.push();
-  p5.translate(rightEye.x, rightEye.y);
-  p5.rotate(a);
-  p5.fill(255);
-  p5.ellipse(0, 0, r, r / 2);
-  p5.fill(0);
-  p5.ellipse(0, 0, r / 4, r / 4);
-  p5.pop();
-} /////////////////////////////////////////////////////////
+  var a = Math.atan2(eyes[0].y - eyes[1].y, eyes[0].x - eyes[1].x); //calc rotation angle for head turning
+  //Draw eyes w/ push & pop
+
+  eyes.forEach(function (eye) {
+    p5.push();
+    p5.translate(eye.x, eye.y);
+    p5.rotate(a);
+    p5.fill(255);
+    p5.ellipse(0, 0, r, r / 2);
+    p5.fill(0);
+    p5.ellipse(0, 0, r / 4, r / 4);
+    p5.pop();
+  });
+} /////////////////////////////OLD CODES/////////////////////////////////////////
 // Draw ellipses over the detected keypoints
 
 
@@ -418,7 +415,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55334" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50200" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/sketch.js
+++ src/sketch.js
@@ -16,7 +16,7 @@ let osc;
 
 //Pose vars
 let preveyePos;
-let prevPose;
+let prevPoses = [];
 
 export function setSketch(sketch) {
   p5 = sketch;
@@ -42,18 +42,6 @@ export function setup() {
   label.position(5, height + 50);
   label.style('font-size', '12pt');
 
-  //init audio button
-  const audioButton = p5.createButton('click to start audio');
-  audioButton.position(5, height + 100);
-  audioButton.mouseClicked(() => {
-    p5.userStartAudio().then(() => {
-      audioButton.remove();
-      osc = new window.p5.Oscillator();
-      // now you can call osc methods to make sound happen
-        osc.start();
-
-    });
-  });
 }
 
 export function draw() {
@@ -62,13 +50,17 @@ export function draw() {
 function drawPoses(poses) {
   p5.translate(width, 0); // move to far corner
   p5.scale(-1.0, 1.0);
-  //p5.tint(255, 100);
+
+  // Uses poses number to alter background
+  let numPoses = poses.length;
+  p5.tint(255, 255/numPoses);
   p5.image(video, 0, 0, video.width, video.height);
   
   //p5.fill(0,10);
-  // p5.rect(0,0,width,height);
+  //p5.rect(0,0,width,height);
 
   readSpeed(poses);
+
   //drawKeypoints(poses);
   //drawSkeleton(poses);
   //playAudio(poses);
@@ -82,28 +74,40 @@ function drawPoses(poses) {
 
 //Function to read and compare speeds of pose keypoints
 function readSpeed(poses){
+  //Debugging to read no. of poses
+  console.log(poses.length);
+
    if(poses.length == 0){ //return if no poses
     return;
   }
 
-  //get vars from pose[0]
-  const pose = poses[0];
+  //(Org testing)get vars from pose[0]
+  //const pose = poses[0];
 
-  if(prevPose == undefined){
-    prevPose = pose;
-    return;
-  }
-  pose.pose.keypoints.forEach(function (keypoint, index){
-    let prevKeypoint = prevPose.pose.keypoints[index];
-    //Calc difference (distance moved)    
-    let dif = Math.abs(prevKeypoint.position.x - keypoint.position.x);
-    let dir; //I'm going to remember how to use vectors(?) and trig to find the direction of movement
-    keypoint.dif = dif; //append to pose
+  //Added poses.foreach loop in order to use multipose detection
+  poses.forEach(function(pose, index){
+    if(prevPoses[index] == undefined){
+      prevPoses[index] = pose;
+      //continue;
+    }
+
+    pose.pose.keypoints.forEach(function (keypoint, index2){
+      let prevKeypoint = prevPoses[index].pose.keypoints[index2];
+      //Calc difference (distance moved)    
+      let dif = prevKeypoint.position.x - keypoint.position.x;
+      let dir; //I'm going to remember how to use vectors(?) and trig to find the direction of movement
+      keypoint.dif = dif; //append this value to Pose
+      //by appening dif will be pased onto the DrawKeypoints to be easily read with a for loop
+    });
+    prevPoses[index] = pose; //set current pose to previous
+
+    // Call Draw Keypoints
+    drawKeypoints(pose); //would like to replace function with "update Keypoints"
   });
-  prevPose = pose; //set current pose to previous
 
 
-  /* //Testing code with just leftEye position
+
+  /* /////////////Old Testing code with just leftEye position////////////
   const eye = pose[1];
   const eyePos = eye.position;
   label.html('LeftEye: ' + JSON.stringify(eyePos));
@@ -115,41 +119,38 @@ function readSpeed(poses){
   let eyedif = Math.abs(preveyePos.x - eyePos.x);
   preveyePos = eyePos;
   */
-
-  drawKeypoints(pose); //would like to replace function with "update Keypoints"
-
-//Next Steps:
-  //Create array of PREVIOUS pose[0](or pose[n] eventually) keypoint positions
-  //Array of vectors {(x,y),(x,y)...}
-  //use a loop to go through this array 
-
-  //instead of "analyzed poses" loop, just append speed,dir+distance to end of org poses array, and pass that to drawKeypoints
+  ////////////////////////////////////////////////////////////////////////
 }
 
 function drawKeypoints(pose) {
-  //pose drawing
+  //Pose drawing/graphic function
 
   pose.pose.keypoints.forEach(function (keypoint,index){
       //Create vars for radius and color from dif values
       let dif = keypoint.dif;
-      let fill = p5.color(5.0*dif,0,10.0*dif);
-      let r = 5.0 + (dif*1.3);
+      let fill = p5.color(5.0*Math.abs(dif),0,10.0*Math.abs(dif)); //Speed determine fill color
+      let r = 5.0 + (dif*1.3); //Size -> Speed direct relationship
 
       if (keypoint.score > 0.5) {
         p5.fill(fill);
         p5.noStroke();
         p5.ellipse(keypoint.position.x, keypoint.position.y, r, r);
+
+        pose.pose.keypoints.forEach((keypoint2) => {
+        p5.stroke(0,0,Math.abs(dif)*10.0);
+        p5.line(keypoint.position.x, keypoint.position.y,keypoint2.position.x,keypoint2.position.y);
+       })
       }
   });
-
-  pose.pose.keypoints.forEach((keypoint) => {
-    pose.pose.keypoints.forEach((keypoint2) => {
-      p5.stroke(0,0,keypoint.dif*10.0);
-      p5.line(keypoint.position.x, keypoint.position.y,keypoint2.position.x,keypoint2.position.y);
-    })
-  })
-
+  // Special Draw Eyes function over top other graphics
   drawEyes(pose);
+  // pose.pose.keypoints.forEach((keypoint) => {
+    
+  // })
+
+  
+
+  /////////////// Old Skeleton Drawing /////////////
   // pose.skeleton.forEach((skeleton) => {
   //       const [p1, p2] = skeleton;
   //       p5.stroke(255, 0, 0);
@@ -160,36 +161,32 @@ function drawKeypoints(pose) {
 
 
 function drawEyes(pose){
-  let leftEye = pose.pose.keypoints[1].position;
-  let rightEye = pose.pose.keypoints[2].position;
-
-  let distance = Math.abs(leftEye.x - rightEye.x);
+  // Create array for both eyes
+  // Check for eyes (error)
+  if(!pose.pose.keypoints[1].position && !pose.pose.keypoints[2].position){
+    return;
+  }
+  let eyes = [pose.pose.keypoints[1].position, pose.pose.keypoints[2].position];
+  let distance = Math.abs(eyes[0].x - eyes[1].x); //calc distance to determine size
   let r = distance/2;
-  let a = Math.atan2(leftEye.y - rightEye.y,leftEye.x - rightEye.x);
+  let a = Math.atan2(eyes[0].y - eyes[1].y, eyes[0].x - eyes[1].x); //calc rotation angle for head turning
 
-  p5.push();
-  p5.translate(leftEye.x,leftEye.y);
-  p5.rotate(a);
-  p5.fill(255);
-  p5.ellipse(0,0, r, r/2);
-  p5.fill(0);
-  p5.ellipse(0,0, r/4, r/4);
-  p5.pop();
-
-  p5.push();
-  p5.translate(rightEye.x,rightEye.y);
-  p5.rotate(a);
-  p5.fill(255);
-  p5.ellipse(0,0, r, r/2);
-  p5.fill(0);
-  p5.ellipse(0,0, r/4, r/4);
-  p5.pop();
-
+  //Draw eyes w/ push & pop
+  eyes.forEach((eye) => {
+    p5.push();
+    p5.translate(eye.x, eye.y);
+    p5.rotate(a);
+    p5.fill(255);
+    p5.ellipse(0,0, r, r/2);
+    p5.fill(0);
+    p5.ellipse(0,0, r/4, r/4);
+    p5.pop();
+  });
 }
 
 
 
-/////////////////////////////////////////////////////////
+/////////////////////////////OLD CODES/////////////////////////////////////////
 // Draw ellipses over the detected keypoints
 function drawKeypointsOld(poses) {
   poses.forEach((pose) =>
